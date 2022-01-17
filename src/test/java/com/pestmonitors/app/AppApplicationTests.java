88d6adfb91d3b6a68a822dce792d167b5d27971c
Xavi Gómez Canals <jumpering@gmail.com>
2022-01-17 22:51:04 +0100

    first commit

---
+++ src/test/java/com/pestmonitors/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pestmonitors.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
